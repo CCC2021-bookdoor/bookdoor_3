9c5cb8113904b1a0a538f52c0e0ef6759bf09668
mikanman <kosp200@gmail.com>
2021-09-21 17:26:44 +0900

    ページネーションを付けた

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from accounts import views
 
 app_name='accounts'
 urlpatterns = [
-    path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('profile/<int:category_id>/<int:page>', views.ProfileView.as_view(), name='profile'),
     path('login/', views.LoginView.as_view(), name='account_login'),
     path('logout/', views.LogoutView.as_view(), name='account_logout'),
     path('signup/', views.SignupView.as_view(), name='account_signup'),
--- accounts/views.py
+++ accounts/views.py
@@ -2,23 +2,27 @@ from random import choices
 from django.views import View
 from django.shortcuts import render, redirect
 from accounts.models import CustomUser, Profile
-from bookdoor.models import BookComment
+from bookdoor.models import BookComment, Category
 from accounts.forms import ProfileForm, SignupUserForm,ProfileCreateForm
 from allauth.account import views
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 import datetime
 
 class ProfileView(LoginRequiredMixin,View):
 
-  def __init__(self,):
+  def __init__(self):
     self.params={
       'books':'',
       'user_data':'',
       'form': '',
       'age':'',
+      'category':Category.objects.all(),
+      'category_id':'',
+      'page_range':'',
     }
 
-  def get(self, request, *args, **kwargs):
+  def get(self, request, category_id, page=1, *args, **kwargs):
     if Profile.objects.filter(owner=request.user).count()==0:
       return redirect(to='/accounts/profile_create')
 
@@ -30,17 +34,43 @@ class ProfileView(LoginRequiredMixin,View):
 
     books=BookComment.objects.filter(writer=request.user)
 
-    self.params['books']=books
+    book=[]
+    if category_id !=0:
+      for item in books:
+        if item.book.category_id == category_id:
+          book.append(item)
+    else:
+      book=books
+
+    book=Paginator(book,12)
+    page_count=book.num_pages
+    page_range=[]
+    if page != page_count and page !=1 :
+      page_range=[page-1,page,page+1]
+    elif page == page_count:
+      for i in range(3):
+        if page-2+i >= 1:
+          page_range.append(page-2+i)
+    else:
+      if page_count >2:
+        page_range=[1,2,3]
+      else:
+        for i in range(page_count):
+          page_range.append(i+1)
+
+    self.params['page_range']=page_range
+    self.params['category_id']=category_id
+    self.params['books']=book.get_page(page)
     self.params['user_data']=user_data
     self.params['form']=ProfileForm(instance=obj)
     self.params['age']=age
     return render(request, 'accounts/profile.html',self.params)
 
-  def post(self, request, *args, **kwargs):
+  def post(self, request, category_id, *args, **kwargs):
     obj=Profile.objects.get(owner=request.user.id)
     profile=ProfileForm(request.POST, instance=obj)
     profile.save()
-    return redirect(to='/accounts/profile')
+    return redirect(to='/accounts/profile/0/0')
 
 
 class LoginView(views.LoginView):
